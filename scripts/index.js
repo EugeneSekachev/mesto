cc2d6e320d24cee57783ac51a8d4f843300cfec5
Eugene Sekachev <sekacheveo@yandex.ru>
2020-07-22 18:51:02 +0500

    сделать проектную работу 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,148 @@
 //объявление переменных
-const openModalButton = document.querySelector('.profile__edit-button');
-const closeModalButton = document.querySelector('.modal__close-button');
-const modal = document.querySelector('.modal');
-const modalForm = modal.querySelector('.modal__form');
-const inputName = document.querySelector('.modal__input_type_name');
-const inputAbout = document.querySelector('.modal__input_type_about');
+//кнопки редактировать и добавить
+const editModalButton = document.querySelector('.profile__edit-button');
+const addModalButton = document.querySelector('.profile__add-button');
+//модалки редактировать, добавить и фото
+const modalEdit = document.querySelector('.modal_type_edit-profile');
+const modalAdd = document.querySelector('.modal_type_add-card');
+const modalPhoto = document.querySelector('.modal_type_photo');
+//кнопки закрытия модалак
+const closeEditModal = modalEdit.querySelector('.modal__close-button');
+const closeAddModal = modalAdd.querySelector('.modal__close-button');
+const closePhotoModal = modalPhoto.querySelector('.modal__close-button');
+//формы
+const modalEditForm = modalEdit.querySelector('.modal__form');
+const modalAddForm = modalAdd.querySelector('.modal__form');
+//инпуты форм edit
+const inputNameEdit = modalEdit.querySelector('.modal__input_type_name');
+const inputAboutEdit = modalEdit.querySelector('.modal__input_type_about');
+//инпуты форм add
+const inputNameAdd = modalAdd.querySelector('.modal__input_type_place ');
+const inputLinkAdd = modalAdd.querySelector('.modal__input_type_link');
+//имя и о себе
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
+//переменная зоны карточек
+const cardBlock = document.querySelector('.elements');
+//template
+const cardTemplate = document.querySelector('.template-card').content;
+//модалка фото(контент)
+const modalPhotoTitle = modalPhoto.querySelector('.modal__photo-title');
+const modalPhotoImg = modalPhoto.querySelector('.modal__photo');
 
+//стартовый массив
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-//функция открытия и закрытия попап
-function toggleModal() {
-  modal.classList.toggle('modal_is-open');
+//функция создания карточек
+const createCard = (data) => {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__text').textContent = data.name;
+  cardElement.querySelector('.element__image').src = data.link;
+
+  cardElement.querySelector('.element__delete').addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove();
+  })
+  cardElement.querySelector('.element__heart').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__heart_active');
+  });
+  cardElement.querySelector('.element__image').addEventListener('click', function (evt) {
+    handlePreviewPhoto(data.name, data.link);
+  });
+
+  return cardElement;
+}
+
+//функция открытия и закрытия модалки
+const toggleModal = (modalWindow) => modalWindow.classList.toggle('modal_is-open');
+
+//функция рендера карточки на страницу
+const renderCard = (data) => cardBlock.prepend(createCard(data));
+
+//стартовые карточки
+const initStartCard = initialCards.reverse().forEach(function (el) {
+  renderCard(el);
+});
+
+//функция открытия модалки-фото
+const handlePreviewPhoto = (name, link) => {
+  modalPhotoTitle.textContent = name;
+  modalPhotoImg.src = link;
+
+  toggleModal(modalPhoto);
 };
+
 //функция возвращение формы
-function openModalForm() {
-  toggleModal();
+const openModalEdit = () => {
+  toggleModal(modalEdit);
 
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
+  inputNameEdit.value = profileName.textContent;
+  inputAboutEdit.value = profileAbout.textContent;
 };
+
+//открытие формы, добавить картинку с пустыми строками
+const openModalAdd = () => {
+  toggleModal(modalAdd);
+  modalAddForm.reset();
+};
+
 //функция сохранение формы
-function submitModalForm() {
+const submitModalFormEdit = (event) => {
   event.preventDefault();
 
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
+  profileName.textContent = inputNameEdit.value;
+  profileAbout.textContent = inputAboutEdit.value;
 
-  toggleModal();
+  toggleModal(modalEdit);
+};
+
+//сохранить карточку
+const submitModalFormAdd = (event) => {
+  event.preventDefault();
+
+  renderCard({ name: inputNameAdd.value, link: inputLinkAdd.value });
+
+  toggleModal(modalAdd);
 };
 
 
 //отправка формы
-modalForm.addEventListener('submit', submitModalForm);
-//открытие и закрытие попапа
-closeModalButton.addEventListener('click', toggleModal);
+modalEditForm.addEventListener('submit', submitModalFormEdit);
+modalAddForm.addEventListener('submit', submitModalFormAdd);
+
 //закрытие попапа через крестик
-openModalButton.addEventListener('click', openModalForm);
+closeEditModal.addEventListener('click', () => toggleModal(modalEdit));
+closeAddModal.addEventListener('click', () => toggleModal(modalAdd));
+closePhotoModal.addEventListener('click', () => toggleModal(modalPhoto));
+
+//открытие и закрытие попапа
+editModalButton.addEventListener('click', openModalEdit);
+addModalButton.addEventListener('click', openModalAdd);
+
 
 
